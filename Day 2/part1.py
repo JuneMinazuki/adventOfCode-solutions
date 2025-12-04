632cf6ae64c3df74fcca6dbdafc8b91fbc0434f4
JuneMinazuki <xtfrancis@gmail.com>
2025-12-04 19:14:31 +0800

    Day 2 solution

---
+++ Day 2/part1.py
@@ -0,0 +1,39 @@
+# Day 2: Part 1
+import math
+
+try:
+    # Variables
+    total = 0
+    ids = list()
+
+    with open("Day 2/input.txt", "r") as file:
+        ids = file.readline().strip().split(",")
+
+        for id in ids:
+            # Get range of number
+            num = id.split("-")
+            numStart = num[0]
+            numEnd = int(num[1])
+
+            # Find where is the half of numStart
+            spiltBy = math.ceil(len(numStart) / 2)
+
+            # Get the first duplicate number to check
+            if (len(numStart) % 2 == 0):
+                checkDuplicate = int(numStart[:spiltBy])
+            else:
+                checkDuplicate = int(pow(10, spiltBy - 1))
+            currentNum = int(f"{checkDuplicate}{checkDuplicate}")
+            
+            while (currentNum <= numEnd):
+                if (currentNum >= int(numStart)):
+                    total += currentNum
+
+                checkDuplicate += 1
+                currentNum = int(f"{checkDuplicate}{checkDuplicate}")
+                
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
+# Output answer
+print(total)
---
+++ Day 2/part2.py
@@ -0,0 +1,45 @@
+# Day 2: Part 2
+import math
+
+try:
+    # Variables
+    total = 0
+    ids = list()
+
+    with open("Day 2/input.txt", "r") as file:
+        ids = file.readline().strip().split(",")
+
+        for id in ids:
+            # Get range of number
+            num = id.split("-")
+            numStart = int(num[0])
+            numEnd = int(num[1])
+            numChecked = set()
+
+            # Find the length of number to test until
+            lengthToTestUntil = math.ceil(len(num[1]) / 2)
+            numToTestUntil = int("9" * lengthToTestUntil)
+            currentNum = 1
+
+            while (currentNum <= numToTestUntil):
+                # Convert currentNum to string and loop for every repeat
+                s_currentNum = str(currentNum)
+                numToRepeat = currentNum
+
+                # Check if currentNum have been tested before
+                if (numToRepeat not in numChecked):
+                    while (numToRepeat <= numEnd):
+                        numToRepeat = int(str(numToRepeat) + s_currentNum)
+
+                        # Check if numToRepeat is in the range
+                        if (numToRepeat >= numStart and numToRepeat <= numEnd):
+                            total += numToRepeat
+                            numChecked.add(numToRepeat)
+                
+                currentNum += 1
+            
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
+# Output answer
+print(total)
