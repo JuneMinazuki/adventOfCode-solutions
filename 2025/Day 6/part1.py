504d74c89227f7fe9e634300f154ac4a3845f85d
JuneMinazuki <xtfrancis@gmail.com>
2025-12-06 15:46:57 +0800

    Day 6 soluion

---
+++ 2025/Day 6/part1.py
@@ -0,0 +1,36 @@
+# Day 6: Part 1
+
+try:
+    # Variables
+    number = []
+    total = 0
+
+    with open("input.txt", "r") as file:
+        content = file.read()
+        lines = content.strip().split("\n") 
+
+        operation = lines[-1].split()
+
+        # Convert each row into list of number
+        for n in range(len(lines) - 1):
+            number.append(lines[n].split())
+
+        # Process each operation column by column
+        for col, operant in enumerate(operation):
+            currentValue = 0 if (operant == '+') else 1
+
+            if (operant == '+'):
+                for i in range(len(number)):
+                    currentValue += int(number[i][col])
+            else:
+                for i in range(len(number)):
+                    currentValue *= int(number[i][col])
+
+            # Add up all answer
+            total += currentValue
+
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
+# Output answer
+print(total)
---
+++ 2025/Day 6/part2.py
@@ -0,0 +1,56 @@
+# Day 6: Part 2
+
+try:
+    # Variables
+    colList = []
+    operation = []
+    number = [[]]
+    total = 0
+
+    with open("input.txt", "r") as file:
+        content = file.read()
+        lines = content.strip().split("\n")
+
+        operation = lines[-1].split()
+
+        # Convert each row into list of char
+        for n in range(len(lines) - 1):
+            colList.append(list(lines[n]))
+
+        # Make sure all row have the same amount of column
+        maxColLength = len(max(colList, key=len))
+        
+        for col in colList:
+            if (len(col) < maxColLength): 
+                col.extend([' '] * (maxColLength - len(colList[n])))
+
+        # Convert each column in a new number
+        for n in range(maxColLength):
+            newNumber = str()
+            for x in range(len(colList)):
+                newNumber += colList[x][n]
+
+            if (newNumber == (" " * len(colList))):
+                number.append([])
+            else:
+                number[-1].append(int(newNumber))
+
+        # Process each operation list by list
+        for col, operant in enumerate(operation):
+            currentValue = 0 if (operant == '+') else 1
+
+            if (operant == '+'):
+                for i in range(len(number[col])):
+                    currentValue += int(number[col][i])
+            else:
+                for i in range(len(number[col])):
+                    currentValue *= int(number[col][i])
+
+            # Add up all answer
+            total += currentValue
+
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
+# Output answer
+print(total)
